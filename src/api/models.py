e0cec5659fc43354698270248bd51b40d6a39436
fajrwajr <bookreaderfajr@gmail.com>
2022-01-27 22:55:18 +0000

    Removed null

--- src/api/models.py
+++ src/api/models.py
@@ -4,11 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(400), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    phone = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(400), unique=False)
+    is_active = db.Column(db.Boolean(), unique=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
