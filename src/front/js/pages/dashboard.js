77a101223aa5e8d3c857fbc4e25cc5c654ef9c6d
fajrwajr <bookreaderfajr@gmail.com>
2022-02-09 21:08:46 +0000

    redesign of main page

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -11,88 +11,90 @@ export const Dashboard = () => {
   actions.getDateData();
   return (
     <>
-      <div className="sidebar">
-        <div class="sidebar-brand">
-          <h2 style={{ fontSize: "150%" }}>
-            <i
-              style={{ marginRight: "1rem" }}
-              class="fa fa-birthday-cake"
-              aria-hidden="true"
-            ></i>
-            Never Forget
-          </h2>
+      <div className="body">
+        <div className="sidebar">
+          <div class="sidebar-brand">
+            <h2 style={{ fontSize: "150%" }}>
+              <i
+                style={{ marginRight: "1rem" }}
+                class="fa fa-birthday-cake"
+                aria-hidden="true"
+              ></i>
+              Never Forget
+            </h2>
+          </div>
+          <div class="sidebar-menu">
+            <ListGroup as="ul">
+              <li>
+                <Link
+                  to="/dashboard"
+                  class="active"
+                  style={{ textDecoration: "none" }}
+                >
+                  <i
+                    style={{ marginRight: "6px" }}
+                    class="fa fa-object-group"
+                    aria-hidden="true"
+                  ></i>
+                  <span>Dashboard</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Add" style={{ textDecoration: "none" }}>
+                  <i
+                    style={{ marginRight: "6px" }}
+                    class="fa fa-users"
+                    aria-hidden="true"
+                  ></i>
+                  <span>Add A Birthday</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Dates" style={{ textDecoration: "none" }}>
+                  <i
+                    style={{ marginRight: "6px" }}
+                    class="fa fa-calendar"
+                    aria-hidden="true"
+                  ></i>
+                  <span>View Birthday</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/giftacard" style={{ textDecoration: "none" }}>
+                  <i
+                    style={{ marginRight: "6px" }}
+                    class="fa fa-gift"
+                    aria-hidden="true"
+                  ></i>
+                  <span>Send A Card</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/sendapayment" style={{ textDecoration: "none" }}>
+                  <i
+                    style={{ marginRight: "6px" }}
+                    class="fa fa-credit-card"
+                    aria-hidden="true"
+                  ></i>
+                  <span>Send A Payment</span>
+                </Link>
+              </li>
+            </ListGroup>
+          </div>
         </div>
-        <div class="sidebar-menu">
-          <ListGroup as="ul">
-            <li>
-              <Link
-                to="/dashboard"
-                class="active"
-                style={{ textDecoration: "none" }}
-              >
-                <i
-                  style={{ marginRight: "6px" }}
-                  class="fa fa-object-group"
-                  aria-hidden="true"
-                ></i>
-                <span>Dashboard</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/Add" style={{ textDecoration: "none" }}>
-                <i
-                  style={{ marginRight: "6px" }}
-                  class="fa fa-users"
-                  aria-hidden="true"
-                ></i>
-                <span>Add A Birthday</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/Dates" style={{ textDecoration: "none" }}>
-                <i
-                  style={{ marginRight: "6px" }}
-                  class="fa fa-calendar"
-                  aria-hidden="true"
-                ></i>
-                <span>View Birthday</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/giftacard" style={{ textDecoration: "none" }}>
-                <i
-                  style={{ marginRight: "6px" }}
-                  class="fa fa-gift"
-                  aria-hidden="true"
-                ></i>
-                <span>Send A Card</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/sendapayment" style={{ textDecoration: "none" }}>
-                <i
-                  style={{ marginRight: "6px" }}
-                  class="fa fa-credit-card"
-                  aria-hidden="true"
-                ></i>
-                <span>Send A Payment</span>
-              </Link>
-            </li>
-          </ListGroup>
-        </div>
-      </div>
-      <div id="error-page">
-        <div class="content">
-          <h2 class="header" data-text="Dashboard">
-            Dashboard
-          </h2>
-          <h4 data-text="Opps! Page not found">
-            You haven't added any users yet!
-          </h4>
-          <p>Head to the Add A Birthday Page to add a user.</p>
-          <div class="btns">
-            <Link to="/Add">Add A Birthday</Link>
-            <Link to="/Dates">View Birthday</Link>
+        <div id="error-page">
+          <div class="content">
+            <h2 class="header" data-text="Dashboard">
+              Dashboard
+            </h2>
+            <h4 data-text="Opps! Page not found">
+              You haven't added any users yet!
+            </h4>
+            <p>Head to the Add A Birthday Page to add a user.</p>
+            <div class="btns">
+              <Link to="/Add">Add A Birthday</Link>
+              <Link to="/Dates">View Birthday</Link>
+            </div>
           </div>
         </div>
       </div>
--- src/front/js/pages/main.js
+++ src/front/js/pages/main.js
@@ -1,16 +1,98 @@
 import React from "react";
-import birthday from "../../img/birthday.jpg";
+import clip from "../../img/vc.png";
+import wave from "../../img/wave1.png";
+import card from "../../img/card.jpg";
+import digit from "../../img/digit.png";
+
 import { Link } from "react-router-dom";
-import "../../styles/home.css";
+import "../../styles/main.css";
 
 export const Main = () => {
   return (
-    <div className="gfg">
-      <img src={birthday} />
-      <h3 class="first-txt">Never forget another birthday again!</h3>
-      <Link to="/register" className="link" style={{ fontSize: "30px" }}>
-        Sign Up
-      </Link>
-    </div>
+    <>
+      <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#">
+            <h4>Don't Forget!</h4>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  SIGN UP
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  LOG IN
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </section>
+
+      <section id="banner">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="promo-title">BEST GIFT SENDER</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                laoreet sem vel tempor venenatis. Praesent tempor ornare porta.
+              </p>
+            </div>
+            <div className="col-md-6 text-center">
+              <img src={clip} className="img-fluid" />
+            </div>
+          </div>
+        </div>
+        <img src={wave} className="bottom-img" />
+      </section>
+
+      <section id="services">
+        <div className="container text-center">
+          <h1 className="title">YOU CAN</h1>
+          <div className="row text-center">
+            <div className="col-md-4 services">
+              <img
+                style={{ height: "60%", marginLeft: "-150px" }}
+                src={card}
+                className="service-img"
+              />
+              <h4 className="heading">Send A Card</h4>
+              <p style={{ marginLeft: "-200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                laoreet sem vel tempor venenatis. Praesent tempor ornare porta.
+              </p>
+            </div>
+
+            <div className="col-md-4 services">
+              <img
+                style={{ height: "60%", marginLeft: "20px" }}
+                src={digit}
+                className="service-img"
+              />
+              <h4 className="headingTwo">Make A Payment</h4>
+              <p style={{ marginRight: "-200px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                laoreet sem vel tempor venenatis. Praesent tempor ornare porta.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
